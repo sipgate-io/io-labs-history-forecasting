98064f1be6a46942edb715b3790efffe31b3cd30
Fabian Siffert <siffert@sipgate.de>
2021-06-24 12:08:00 +0200

    wip: areacode extraction

--- history-fetch-all.js
+++ history-fetch-all.js
@@ -20,17 +20,36 @@ historyModule
 		{ types: ['SMS', 'FAX', 'CALL'], directions: ['INCOMING'] }
 	)
 	.then((historyElements) => {
+
 		// sicherstellen, dass alle nummer mit +49 anfangen
-
-
 		// Ländervorwahlen entfernen
+		historyElements.forEach((e) => {
+			let number = e.source;
+			if(!number.startsWith('+49')){
+				return
+			}
+			let areaCodeLengthTwo, areaCodeLengthThree, areaCodeLengthFour;
+			number = number.substring(3);
 
+			//Die Vorwahlen zu isolieren hat sich als sehr schwer herausgestellt, da die übergebene Nummer keine Formatierung hat.
+			//Da es Vorwahlen gibt, die sich stark ähneln kann es zu Überschneidungen kommen. Z.B.: 212 Solingen und 2129 Haan Rheinl.
+
+			areaCodeLengthTwo = number.substring(2);
+			areaCodeLengthThree = number.substring(3);
+			areaCodeLengthFour = number.substring(4);
+			areaCodeMap = parseCsv();
+			if(areaCodeLengthTwo in  areaCodeMap){
+				//areaCodeMap[areaCodeLengthTwo] 
+			}
+
+			
+		})
 
 		// Vorwahl ermitteln
 
 
 		// gefundene Vorwahlen in ein Objekt speichern und inkrementieren
-
+		//console.log(historyElements);
 	})
 	.catch(console.error);
 
@@ -40,7 +59,7 @@ function parseCsv(){
 	const allAreaCodes = fs.readFileSync('./area_codes.csv').toString().split("\n");
 	for (const line of allAreaCodes.slice(1,-1)) {
 		let [areaCode, city] = line.split(";")
-		areaCodeMap[areaCode] = city;
+		areaCodeMap[areaCode] = {city: city, occurences: 0};
 	}
 	return areaCodeMap;
 }
