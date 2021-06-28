0cceeca584801e0e2d9c86765fabb1afb39d457d
Julian Bokelmann <bokelmann@sipgate.de>
2021-06-28 10:21:05 +0200

    feat: refactor and create csv from data
    Co-authored-by: Benjamin Awad <awad@sipgate.de>
    Co-authored-by: Daniel Knapp <knapp@sipgate.de>
    Co-authored-by: David Borzek <Borzek@sipgate.de>

--- history-fetch-all.js
+++ history-fetch-all.js
@@ -10,60 +10,80 @@ const personalAccessToken = process.env.SIPGATE_TOKEN || '';
  * For details on how to instantiate the client, see 'examples/client/client.ts'
  */
 const client = sipgateIO({
-	tokenId: personalAccessTokenId,
-	token: personalAccessToken,
+    tokenId: personalAccessTokenId,
+    token: personalAccessToken,
 });
 const historyModule = createHistoryModule(client);
-let areaCodeMap = parseCsv();
+let areaCodeMap = parseAreaCodeCsv();
 const areaCodeMatch = [];
 
 historyModule
-	.fetchAll(
-		{ types: ['SMS', 'FAX', 'CALL'], directions: ['INCOMING'] },
-		{offset:0, limit:100}
-	)
-	.then((historyElements) => {
+    .fetchAll(
+        { types: ['SMS', 'FAX', 'CALL'], directions: ['INCOMING'] },
+        { offset: 0, limit: 100 }
+    )
+    .then((historyElements) => {
+        historyElements.forEach((entry) => {
+            let number = entry.source;
+            if (!isCountryNumber('+49', number)) {
+                return;
+            }
+            number = number.substring(3);
 
-		historyElements.forEach((e) => {
-			let number = e.source;
-			if(!number.startsWith('+49')){
-				return
-			}
-			let areaCodeLengthTwo, areaCodeLengthThree, areaCodeLengthFour;
-			number = number.substring(3);
+            for (let i = 5; i > 1; i--) {
+                const areaCode = number.substr(0, i);
+                if (areaCodeMap[areaCode]) {
+                    areaCodeMap[areaCode].occurences += 1;
+                    break;
+                }
+            }
+        });
 
-			areaCodeLengthTwo = number.substr(0,2);
-			areaCodeLengthThree = number.substr(0,3);
-			areaCodeLengthFour = number.substr(0,4);
+        Object.keys(areaCodeMap).forEach((key) => {
+            if (areaCodeMap[key].occurences > 0) {
+                areaCodeMatch.push({ areacode: key, ...areaCodeMap[key] });
+            }
+        });
+        areaCodeMatch.sort((firstCity, secondCity) => {
+            return secondCity.occurences - firstCity.occurences;
+        });
+        console.log(areaCodeMatch);
+        createForecastCsv(areaCodeMatch);
+    })
+    .catch(console.error);
 
-			if(areaCodeMap[areaCodeLengthFour]){
-				areaCodeMap[areaCodeLengthFour].occurences +=1;
-			}else if(areaCodeMap[areaCodeLengthThree]) {
-				areaCodeMap[areaCodeLengthThree].occurences += 1;
-			}
-			else if(areaCodeMap[areaCodeLengthTwo]){
-				areaCodeMap[areaCodeLengthTwo].occurences +=1;
-			}
-		})
+function parseAreaCodeCsv() {
+    const areaCodeMap = {};
+    const allAreaCodes = fs
+        .readFileSync('./area_codes.csv')
+        .toString()
+        .split('\n');
+    for (const line of allAreaCodes.slice(1, -1)) {
+        let [areaCode, city] = line.split(';');
+        areaCodeMap[areaCode] = { city: city, occurences: 0 };
+    }
+    return areaCodeMap;
+}
 
-		Object.keys(areaCodeMap).forEach(function(key) {
-			if(areaCodeMap[key].occurences > 0){
-				areaCodeMatch.push({ areacode:key, ...areaCodeMap[key]})
-			}
-		});
-		areaCodeMatch.sort(function(firstCity,secondCity){
-			return secondCity.occurences - firstCity.occurences;
-		})
-		console.log(areaCodeMatch)
-	})
-	.catch(console.error);
+function isCountryNumber(countryNumber, number) {
+    return number.startsWith(countryNumber);
+}
 
-function parseCsv(){
-	const areaCodeMap = {}
-	const allAreaCodes = fs.readFileSync('./area_codes.csv').toString().split("\n");
-	for (const line of allAreaCodes.slice(1,-1)) {
-		let [areaCode, city] = line.split(";")
-		areaCodeMap[areaCode] = {city: city, occurences: 0};
-	}
-	return areaCodeMap;
+function createForecastCsv(areaCodeMatch) {
+    const rows = [['areacode', 'city', 'occurences']];
+    const csvString = [
+        ['areacode', 'city', 'occurences'],
+        ...areaCodeMatch.map((item) => [
+            item.areacode,
+            item.city,
+            item.occurences,
+        ]),
+    ]
+        .map((e) => e.join(','))
+        .join('\n');
+
+    fs.writeFile('forecast.csv', csvString, (err) => {
+        if (err) return console.log(err);
+        console.log('Created forecast.csv');
+    });
 }
